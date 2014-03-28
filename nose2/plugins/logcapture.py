cfed3263efa27d756a931e9be4f304cb308f650e
Jason Madden <jamadden@gmail.com>
2014-03-28 13:01:32 -0500

    Also don't let TypeErrors during log formatting kill the process.

--- nose2/plugins/logcapture.py
+++ nose2/plugins/logcapture.py
@@ -101,7 +101,7 @@ class LogCapture(Plugin):
         for r in self.handler.buffer:
             try:
                 records.append( format(r) )
-            except AttributeError:
+            except (AttributeError,TypeError):
                 records.append( "FAILED RECORD %r" % r )
         if 'logs' in event.metadata:
             event.metadata['logs'].extend(records)
